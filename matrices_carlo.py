1329eec5c1b195411163a2cb143c289cbc675fb9
Eduardo ugb <smss061022@ugb.edu.sv>
2022-08-02 21:23:49 -0600

    Usando numpy, el programa es capaz de generar las matrices, sacar la matriz inversa y sacar la matriz con el mensaje decodificado. Pero con una matriz inversa con números flotantes, como la mía, el mensaje decodificado aparece en notación científica, sería de pasarlo a nominal.

--- matrices_carlo.py
+++ matrices_carlo.py
@@ -1,29 +1,32 @@
+#Esta es una función principal, no es necesaria para este ejercicio
+#pero aprendí que es una buena práctica
 def run():
+    import numpy as np
 
     def fill():
-        m = int(input('INGRESE EL NUMERO DE FILAS:'))
-        n = int(input('INGRESE EL NUMERO DE COLUMNAS:'))
-        a = []
-        for i in range(m):
-            b = []
-            for j in range(n):
-                 b.append(int(input(f'Ingrese el elemento {(i+1,j+1)}:')))
-            a.append(b)
-        return a
+        n = int(input('Ingrese el número de filas:'))
+        m = int(input('Ingrese el número de columnas: '))
+        print('Ingrese los elementos de la matriz, de izquierda a derecha en orden descendente: ')
+        list=[]
+        for i in range(n*m):
+            num = float(input(f'Elemento {i+1}: '))
+            list.append(num)
+        array = np.array(list)
+        array = array.reshape(n,m)
+        return array
 
-    def determinant_m():
-        pass
+    print('Ingrese la matriz mensaje:\n ')
+    message = fill()
+    print('\nIngrese la matriz encriptación (M):\n ')
+    crypt_m = fill()
+    reverse_m = np.linalg.inv(crypt_m)
+    print(f'La matriz invversa de M es:\n {reverse_m}')
+    identity = np.dot(crypt_m, reverse_m)
+    print(f'La matriz identidad de M por inversa de M es:\n {identity}')
+    decrypt_message = np.dot(reverse_m, message)
+    print(f'La matriz con el mensaje decodificado es:\n {decrypt_message}')
 
 
-    print('INGRESE LA MATRIZ CON MENSAJE ENCRIPTADO:')
-    crypt_message = fill()
-    print(crypt_message)
-    print('INGRESE LA MATRIZ DE DESENCRIPTADO (M):')
-    decrypt = fill()
-    print(decrypt)
-    # print('INGRESE LA MATRIZ IDENTIDAD DE M:')
-    # identity: fill()
-    
-    
+#Aquí es donde se corre la función, repito, no es necesario pero es una buena práctica
 if __name__ == '__main__':
     run()

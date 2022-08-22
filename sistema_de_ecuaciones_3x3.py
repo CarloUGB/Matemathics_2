ab4209563849ee221847f0cb678a9c33a118127e
Eduardo ugb <smss061022@ugb.edu.sv>
2022-08-21 21:49:58 -0600

    Sistema de ecuaciones 3 x 3 sin numpy completado

--- sistema_de_ecuaciones_3x3.py
+++ sistema_de_ecuaciones_3x3.py
@@ -1,14 +1,19 @@
 from fractions import Fraction
+#no uso Fraction porque muestra número muy grandes en las fracciones
+#si quieres usarlo, solo agregalo en x,y, z
 
 def run():
     def llenado():
         m = int(input("Ingrese el numero de filas de la matriz: "))
+        print("_______________________________________________")
         n = int(input("Ingrese el numero de columnas de la matriz: "))
+        print("_______________________________________________")
         a=[]
         for i in range(m):
             f=[]
             for j in range(n):
                 f.append(float(input(f'Ingrese el elemento {(i+1,j+1)}:')))
+                print("_______________________________________________")
             a.append(f)   
         return a
 
@@ -28,26 +33,32 @@ def run():
     a11X1 + a12X2 + a13X3 = C1
     a21X2 + a22X2 + a23X2 = C2
     a31X3 + a32X3 + a33X3 = C3""")
+    print("_______________________________________________")
 
-    print("Ingrese la matriz de incógnitas (a11, a12, a13, a21 ...): ")
+
+    print("Ingrese la matriz de coeficientes (a11, a12, a13, a21 ...): ")
+    print("_______________________________________________") 
     delta = llenado()
     det_delta = determinante(delta)
 
     print("Ingrese la matriz para la incógnita 1 (x): ")
+    print("_______________________________________________")
     delta_x = llenado()
     det_delta_x = determinante(delta_x)
 
     print("Ingrese la matriz para la incógnita 2 (y) : ")
+    print("_______________________________________________")
     delta_y = llenado()
     det_delta_y = determinante(delta_y)
     
     print("Ingrese la matriz para la incógnita 3 (z) : ")
+    print("_______________________________________________")
     delta_z = llenado()
     det_delta_z = determinante(delta_z)
 
-    x = Fraction(det_delta_x / det_delta)
-    y = Fraction(det_delta_y / det_delta)
-    z = Fraction(det_delta_z / det_delta)
+    x = det_delta_x / det_delta
+    y = det_delta_y / det_delta
+    z = det_delta_z / det_delta
 
     print(f'X es igual a {x}; Y es igual a {y}; Z es igual a {z}')
 
